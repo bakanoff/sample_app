ae435e53c8f026bf0ed468ec9bdba29701cb372c
Vitalik Bakanoff <mrmaster@ukr.net>
2018-01-17 14:11:28 +0200

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -15,9 +15,9 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
-      end
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
     end
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
@@ -29,5 +29,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_select 'a', text: 'delete', count: 0
   end
+
+ # test "should not show non activated user" do
+ #   log_in_as(@non_admin)
+  #  get user_path(@non_activated)
+  #  assert_redirected_to root_path
+ # end
 
 end
